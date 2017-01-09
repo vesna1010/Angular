7dc8b7516ac121a49fc55e742fc808235620a9c3
vesna1010 <vesnasdoboj@gmail.com>
2017-01-09 02:30:34 +0100

    Update my-filter.js

--- Students/src/main/webapp/resource/angular/my-filter.js
+++ Students/src/main/webapp/resource/angular/my-filter.js
@@ -1,15 +1,16 @@
-myApp.filter("filterCourse", function(){
+myApp.filter("filterDepartment", function(){
 	return function(items, name){
 		var newItems=[];
-		if(name=="All Courses"){
+		if(name=="All Departments"){
 			return items;
 		}
 		angular.forEach(items, function(item){
-			if(item.course.courseName==name){
+			if(item.department.departmentName==name){
 				newItems.push(item);
 			}
 		});
 		return newItems;
 	}
 });
+
 
