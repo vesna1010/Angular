d0ce1eab680c318f6b49ed44caf347bb7fca55bb
vesna1010 <vesnasdoboj@gmail.com>
2017-01-09 02:28:05 +0100

    Update DepartmentServiceImpl.java

--- Students/src/main/java/students/service/imp/DepartmentServiceImpl.java
+++ Students/src/main/java/students/service/imp/DepartmentServiceImpl.java
@@ -3,34 +3,34 @@ package students.service.imp;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import students.dao.CourseDao;
-import students.model.Course;
-import students.service.CourseService;
+import students.model.*;
+import students.dao.DepartmentDao;
+import students.service.DepartmentService;
 
 @Service
-public class CourseServiceImpl implements CourseService {
+public class DepartmentServiceImpl implements DepartmentService {
 	
 	@Autowired
-	private CourseDao courseDao;
+	private DepartmentDao courseDao;
 
 	@Override
-	public Course getCourseById(long id) {
-		return courseDao.getCourseById(id);
+	public Department getDepartmentById(long id) {
+		return courseDao.getDepartmentById(id);
 	}
 
 	@Override
-	public List<Course> getCourses() {
-		return courseDao.getCourses();
+	public List<Department> getDepartments() {
+		return courseDao.getDepartments();
 	}
 
 	@Override
-	public void saveOrUpdateCourse(Course course) {
-		courseDao.saveOrUpdateCourse(course);
+	public void saveOrUpdateDepartment(Department department) {
+		courseDao.saveOrUpdateDepartment(department);
 	}
 
 	@Override
-	public void deleteCourse(long id) {
-		courseDao.deleteCourse(id);
+	public void deleteDepartment(long id) {
+		courseDao.deleteDepartment(id);
 	}
 
 }
