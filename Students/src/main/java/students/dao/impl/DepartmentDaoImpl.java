03dcf8e146e1062fd1a7ed1fd4100cdb71667394
vesna1010 <vesnasdoboj@gmail.com>
2017-01-09 02:25:28 +0100

    Update DepartmentDaoImpl.java

--- Students/src/main/java/students/dao/impl/DepartmentDaoImpl.java
+++ Students/src/main/java/students/dao/impl/DepartmentDaoImpl.java
@@ -7,12 +7,12 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import students.dao.CourseDao;
-import students.model.Course;
+import students.dao.DepartmentDao;
+import students.model.*;
 
 @Transactional
 @Component
-public class CourseDaoImpl implements CourseDao {
+public class DepartmentDaoImpl implements DepartmentDao{
 	
 	@Autowired
 	private SessionFactory session;
@@ -22,35 +22,27 @@ public class CourseDaoImpl implements CourseDao {
 	}
 	
 	@Override
-	public Course getCourseById(long id) {
-		Query query=getSession().createQuery("from Course where id=:id");
+	public Department getDepartmentById(long id) {
+		Query query=getSession().createQuery("from Department where id=:id");
 		query.setLong("id", id);
-		return (Course) query.uniqueResult();
+		return (Department) query.uniqueResult();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<Course> getCourses() {
-		Query query=getSession().createQuery("from Course");
+	public List<Department> getDepartments() {
+		Query query=getSession().createQuery("from Department");
 		return query.list();
 	}
 
 	@Override
-	public void saveOrUpdateCourse(Course course) {
-		 Course course1=getCourseById(course.getId());
-		if(course1==null)
-			getSession().save(course);
-		else{
-			Query query=getSession().createQuery("UPDATE Course set courseName=:courseName where id=:id");
-			query.setParameter("courseName", course.getCourseName());
-			query.setParameter("id", course.getId());
-			query.executeUpdate();
-		}
+	public void saveOrUpdateDepartment(Department department) {
+		getSession().saveOrUpdate(department);
 	}
 
 	@Override
-	public void deleteCourse(long id) {
-		getSession().delete(getCourseById(id));
+	public void deleteDepartment(long id) {
+		getSession().delete(getDepartmentById(id));
 	}
 
 }
