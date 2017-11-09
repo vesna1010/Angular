a77eb171bd3a0a0fb3409a44a607b62e86eb1d8c
Vesna S <vesnasdoboj@gmail.com>
2017-11-09 09:28:23 +0100

    Update DepartmentController.js

--- Students/src/main/webapp/resource/js/controller/DepartmentController.js
+++ Students/src/main/webapp/resource/js/controller/DepartmentController.js
@@ -1,31 +1,31 @@
-app.controller("DepartmentsController", 
-		function($scope, $rootScope, departments) {
-	
-	$rootScope.activeTab = 'departments';
-	$scope.departments = departments;
-	
-});
+app.controller('DepartmentsController', 
+	       ['$scope', '$rootScope', 'departments', function($scope, $rootScope, departments) {
+		       
+		       $rootScope.activeTab = 'departments';
+		       $scope.departments = departments;
+	       }]);
 
 app.controller("OneDepartmentController", 
+	       ['$scope', '$rootScope', '$location', 'DepartmentFactory', 'department',
 		function($scope, $rootScope, $location, DepartmentFactory, department) {
-	
-	$rootScope.activeTab = 'departments';
-	$scope.department = department;
-	
-	$scope.saveOrUpdateDepartment = function(department) {
-		var func = department.id == undefined ? DepartmentFactory.saveDepartment :
-			DepartmentFactory.updateDepartment;
-		
-		func(department).success(function(message) {
-			showMessage(message);
-		});
-	}
-	
-	function showMessage(message) {
-		alert(message);
-		$location.path("/departments");
-	}
-});
+			
+			$rootScope.activeTab = 'departments';
+			$scope.department = department;
+			
+			$scope.saveOrUpdateDepartment = function(department) {
+				var func = department.id == undefined ? DepartmentFactory.saveDepartment :
+				DepartmentFactory.updateDepartment;
+				
+				func(department).success(function(message) {
+					showMessage(message);
+				});
+			}
+			
+			function showMessage(message) {
+				alert(message);
+				$location.path("/departments");
+			}
+		}]);
 
 
 
