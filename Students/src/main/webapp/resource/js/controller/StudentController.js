ebb098146b37fe9aa31a90994b967e2d35f84201
Vesna S <vesnasdoboj@gmail.com>
2017-11-09 09:31:11 +0100

    Update StudentController.js

--- Students/src/main/webapp/resource/js/controller/StudentController.js
+++ Students/src/main/webapp/resource/js/controller/StudentController.js
@@ -1,31 +1,28 @@
-app.controller('StudentsController', [ '$scope', '$rootScope', 'students', 'departments',
+app.controller('StudentsController', 
+	       [ '$scope', '$rootScope', 'students', 'departments',
 		function($scope, $rootScope, students, departments) {
-	
-	$rootScope.activeTab = 'students';
-	$scope.students = students;
-	$scope.departments = departments;
-	$scope.name = "All Departments";
-	
-}]);
+			$rootScope.activeTab = 'students';
+			$scope.students = students;
+			$scope.departments = departments;
+			$scope.name = "All Departments";
+		}]);
 
-app.controller("OneStudentController", [ '$scope', '$rootScope', '$location', 'StudentFactory', 
-	'student', 'departments', function($scope, $rootScope, $location, StudentFactory, student, departments) {
-	
-	$rootScope.activeTab = 'students';
-	$scope.student = student;
-	$scope.departments = departments;
-	
-	$scope.saveOrUpdateStudent = function(student) {
-		var func = student.id == undefined ? StudentFactory.saveStudent :
-			StudentFactory.updateStudent;
-		
-		func(student).success(function(message) {
-			showMessage(message);
-		});
-	}
-	
-	function showMessage(message) {
-		alert(message);
-		$location.path("/students");
-	}
-}]);
+app.controller("OneStudentController", 
+	       [ '$scope', '$rootScope', '$location', 'StudentFactory', 'student', 'departments', 
+		function($scope, $rootScope, $location, StudentFactory, student, departments) {
+			$rootScope.activeTab = 'students';
+			$scope.student = student;
+			$scope.departments = departments;
+			
+			$scope.saveOrUpdateStudent = function(student) {
+				var func = student.id == undefined ? StudentFactory.saveStudent : StudentFactory.updateStudent;
+				func(student).success(function(message) {
+					showMessage(message);
+				});
+			}
+			
+			function showMessage(message) {
+				alert(message);
+				$location.path("/students");
+			}
+		}]);
