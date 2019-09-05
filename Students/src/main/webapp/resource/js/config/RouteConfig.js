8b78d60768262a643c243b81900c25271515d77c
Vesna <vesnasdoboj@gmail.com>
2019-09-05 04:56:01 +0200

    Update RouteConfig.js

--- Students/src/main/webapp/resource/js/config/RouteConfig.js
+++ Students/src/main/webapp/resource/js/config/RouteConfig.js
@@ -1,99 +1,68 @@
-var app = angular.module("app", ['ngRoute']);
+var app = angular.module('app', [ 'ngRoute' ]);
 
-app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider
-    .when('/', {
-    	template : '<h1 class="text-center">HOME PAGE</h1>',
-    	controller : 'HomeController'
-    })
-    .when('/departments', {
-    	templateUrl : '/Students/resources/templates/departments.html',
-	    controller : 'DepartmentsController',
-	    resolve : {
+app.config([ '$routeProvider', function($routeProvider) {
+	
+	$routeProvider.when('/', {
+		template : '<h1 class="text-center">HOME PAGE</h1>',
+		controller : 'HomeController'
+	});
+	
+	$routeProvider.when('/departments', {
+		templateUrl : 'resources/templates/departments.html',
+		controller : 'DepartmentsPageController',
+		resolve : {
 			departments : function(DepartmentFactory) {
 				return DepartmentFactory.findAllDepartments();
 			}
 		}
-    })
-    .when('/addDepartment', {
-    	templateUrl : '/Students/resources/templates/departmentForm.html',
-	    controller : 'OneDepartmentController',
-	    resolve : {
-			department : function() {
-				return {};
+	});
+	
+	$routeProvider.when('/departments/form/:id?', {
+		templateUrl : 'resources/templates/departmentForm.html',
+		controller : 'DepartmentsFormController',
+		resolve : {
+			department : function(DepartmentFactory, $route) {
+				var id = $route.current.params.id;
+				
+				return (angular.isDefined(id) ? DepartmentFactory.findDepartmentById(id) : {});
 			}
 		}
-    })
-    .when('/findDepartment/:id', {
-    	templateUrl : '/Students/resources/templates/departmentForm.html',
-	    controller : 'OneDepartmentController',
-	    resolve : {
-			department : function(DepartmentFactory) {
-				return DepartmentFactory.findDepartmentById();
-			}
-		}
-    })
-    .when('/deleteDepartment/:id', {
-    	templateUrl : '/Students/resources/templates/departments.html',
-	    controller : 'DepartmentsController',
-	    resolve : {
+	});
+	
+	$routeProvider.when('/students', {
+		templateUrl : 'resources/templates/students.html',
+		controller : 'StudentsPageController',
+		resolve : {
 			departments : function(DepartmentFactory) {
-				return DepartmentFactory.deleteDepartmentById();
-			}
-		}
-    })
-    .when('/students', {
-    	templateUrl : '/Students/resources/templates/students.html',
-	    controller : 'StudentsController',
-	    resolve : {
-	    	students : function(StudentFactory) {
-				return StudentFactory.findAllStudents();
+				return DepartmentFactory.findAllDepartments();
 			},
-			departments : function(DepartmentFactory) {
-				return DepartmentFactory.findAllDepartments();
-			}
-		}
-    })
-     .when('/addStudent', {
-    	templateUrl : '/Students/resources/templates/studentForm.html',
-	    controller : 'OneStudentController',
-	    resolve : {
-	    	 student : function() {
-	 	    	return {};
-	 	    },
-	 	    departments : function(DepartmentFactory) {
-	 			return DepartmentFactory.findAllDepartments();
-	 		}
-	    }
-    })
-    .when('/findStudent/:id', {
-    	templateUrl : '/Students/resources/templates/studentForm.html',
-	    controller : 'OneStudentController',
-	    resolve : {
-			student : function(StudentFactory) {
-				return StudentFactory.findStudentById();
-			},
-			departments : function(DepartmentFactory) {
-				return DepartmentFactory.findAllDepartments();
-			}
-		}
-    })
-    .when('/deleteStudent/:id', {
-    	templateUrl : '/Students/resources/templates/students.html',
-	    controller : 'StudentsController',
-	    resolve : {
 			students : function(StudentFactory) {
-				return StudentFactory.deleteStudentById();
-			},
-			departments : function(DepartmentFactory) {
-				return DepartmentFactory.findAllDepartments();
+				return StudentFactory.findAllStudents();
 			}
 		}
-    })
-    .otherwise({
-			redirectTo : "/"
-	})
-	.caseInsensitiveMatch = true;
-}]);
+	});
+	
+	$routeProvider.when('/students/form/:id?', {
+		templateUrl : 'resources/templates/studentForm.html',
+		controller : 'StudentsFormController',
+		resolve : {
+			departments : function(DepartmentFactory) {
+				return DepartmentFactory.findAllDepartments();
+			},
+			student : function(StudentFactory, $route) {
+				var id = $route.current.params.id;
+				
+				return (angular.isDefined(id) ? StudentFactory.findStudentById(id) : {});
+			}
+		}
+	});
+	
+	$routeProvider.otherwise({
+		redirectTo : '/'
+	});
+	
+	$routeProvider.caseInsensitiveMatch = true;
+	
+} ]);
 
 
