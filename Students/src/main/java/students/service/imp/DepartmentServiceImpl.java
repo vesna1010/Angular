4e00ebb2425ee0d61db5d45d7b4ac1ac89598bb0
Vesna <vesnasdoboj@gmail.com>
2019-09-05 04:54:36 +0200

    Update DepartmentServiceImpl.java

--- Students/src/main/java/students/service/imp/DepartmentServiceImpl.java
+++ Students/src/main/java/students/service/imp/DepartmentServiceImpl.java
@@ -2,25 +2,31 @@ package students.service.imp;
 
 import java.util.List;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import students.model.*;
 import students.repository.DepartmentRepository;
 import students.service.DepartmentService;
 
 @Service
+@Transactional
 public class DepartmentServiceImpl implements DepartmentService {
 
-	
 	@Inject
 	private DepartmentRepository departmentRepository;
 
 	@Override
 	public List<Department> findAllDepartments() {
+
 		return departmentRepository.findAll();
 	}
 
 	@Override
 	public Department findDepartmentById(Long id) {
+		if (!departmentRepository.existsById(id)) {
+			throw new RuntimeException("No department with id " + id);
+		}
+
 		return departmentRepository.findOne(id);
 	}
 
@@ -30,13 +36,22 @@ public class DepartmentServiceImpl implements DepartmentService {
 	}
 
 	@Override
-	public void updateDepartment(Department department) {
+	public void updateDepartment(Department department, Long id) {
+		if (!departmentRepository.existsById(id)) {
+			throw new RuntimeException("No department with id " + id);
+		}
+
 		departmentRepository.update(department);
 	}
 
 	@Override
 	public void deleteDepartmentById(Long id) {
+		if (!departmentRepository.existsById(id)) {
+			throw new RuntimeException("No department with id " + id);
+		}
+
 		departmentRepository.deleteById(id);
 	}
 
 }
+
