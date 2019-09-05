1898a6633916fe49bdddb4296f3add525c650bd6
Vesna <vesnasdoboj@gmail.com>
2019-09-05 04:53:40 +0200

    Update DepartmentService.java

--- Students/src/main/java/students/service/DepartmentService.java
+++ Students/src/main/java/students/service/DepartmentService.java
@@ -1,20 +1,18 @@
 package students.service;
 
 import java.util.List;
-
 import students.model.Department;
 
 public interface DepartmentService {
-	
-	
+
 	List<Department> findAllDepartments();
-	
+
 	Department findDepartmentById(Long id);
 
 	void saveDepartment(Department department);
-	
-	void updateDepartment(Department department);
-	
+
+	void updateDepartment(Department department, Long id);
+
 	void deleteDepartmentById(Long id);
 
 }
