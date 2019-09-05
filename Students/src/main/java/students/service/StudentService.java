f94ddb5261951d78b31bdd81ea1c0f5970a6a045
Vesna <vesnasdoboj@gmail.com>
2019-09-05 04:54:06 +0200

    Update StudentService.java

--- Students/src/main/java/students/service/StudentService.java
+++ Students/src/main/java/students/service/StudentService.java
@@ -1,20 +1,19 @@
 package students.service;
 
 import java.util.List;
-
 import students.model.Student;
 
 public interface StudentService {
 
-	
 	List<Student> findAllStudents();
-	
+
 	Student findStudentById(Long id);
 
 	void deleteStudentById(Long id);
 
 	void saveStudent(Student student);
-	
-	void updateStudent(Student student);
-	
+
+	void updateStudent(Student student, Long id);
+
 }
+
