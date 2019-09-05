bf0caefb2657cf345dedc539041fcc71d39a3b14
Vesna <vesnasdoboj@gmail.com>
2019-09-05 04:59:30 +0200

    Update StudentFactory.js

--- Students/src/main/webapp/resource/js/factory/StudentFactory.js
+++ Students/src/main/webapp/resource/js/factory/StudentFactory.js
@@ -1,34 +1,44 @@
-app.factory('StudentFactory', function($http, $route) {
+var app = angular.module('app');
+
+app.factory('StudentFactory', function($http) {
+	
 	return {
-		findAllStudents : function() {
-			return $http.get('findAllStudents').then(function(response) {
-				return response.data;
-			});	
-		},
-		findStudentById : function() {
-			return $http.get('findStudent/' + $route.current.params.id).then(function(response) {
-				return response.data;
-			});
-		},
-		saveStudent : function(student) {
-			return $http.post('saveStudent', JSON.stringify(student), {
-				responseType:'text',
-				transformResponse: angular.identity
-			});		
-		},
-		updateStudent : function(student) {
-			return $http.put('updateStudent', JSON.stringify(student), {
-				responseType:'text',
-				transformResponse: angular.identity
-			});		
-		},
-		deleteStudentById : function() {
-			return $http.delete('deleteStudent/'+ $route.current.params.id, {
-				responseType : 'json',
-				transformResponse: angular.identity,
-			}).then(function(response) {
-				return response.data;
-			});	
-		}
+		findAllStudents : findAllStudents,
+		findStudentById : findStudentById,
+		saveStudent : saveStudent,
+		updateStudent : updateStudent,
+		deleteStudentById : deleteStudentById
+	};
+	
+	function findAllStudents() {
+		return $http.get('students').then(function(response){
+			return response.data;
+		});		
 	}
+	
+	function findStudentById(id) {
+		return $http.get('students/' + id).then(function(response){
+			return response.data;
+		});	
+	}
+	
+	function saveStudent(student) {
+		return $http.post('students', student).then(function(response){
+			alert(response.data);
+		});		
+	}
+	
+	function updateStudent(student, id) {
+		return $http.put('students/' + id, student).then(function(response){
+			alert(response.data);
+		});		
+	}
+	
+	function deleteStudentById(id) {
+		return $http.delete('students/'+ id).then(function(response){
+			alert(response.data);
+		});		
+	}
+	
 });
+
