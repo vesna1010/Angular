2654c58760b32da193e21c048f51a0d1ec78e5dd
Vesna <vesnasdoboj@gmail.com>
2019-09-05 04:49:13 +0200

    Update Department.java

--- Students/src/main/java/students/model/Department.java
+++ Students/src/main/java/students/model/Department.java
@@ -10,23 +10,23 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-@SuppressWarnings("serial")
 @Entity
 public class Department implements Serializable {
 
-	@Id 
+	private static final long serialVersionUID = 1L;
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String title;
-	@JsonIgnore 
+	@JsonIgnore
 	@OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
 	private List<Student> students;
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -40,6 +40,28 @@ public class Department implements Serializable {
 
 	public List<Student> getStudents() {
 		return students;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 }
